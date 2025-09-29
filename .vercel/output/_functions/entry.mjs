3d1149ae4be32ddac2b128f56158c2806a12e453
Steven Aerts <stevenaerts@me.com>
2025-09-29 17:22:23 +0200

    production fixes 3

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CMVePzp7.mjs';
-import { manifest } from './manifest_5XXSeWHG.mjs';
+import { manifest } from './manifest_Dd7YcX9_.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -45,7 +45,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "794e62c3-84d4-4b1c-bab4-8f5f39021523",
+    "middlewareSecret": "7febebb5-2469-419a-a254-6dce2e818d3d",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
